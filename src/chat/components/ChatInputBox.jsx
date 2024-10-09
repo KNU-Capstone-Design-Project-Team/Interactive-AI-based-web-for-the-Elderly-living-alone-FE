8621aee64f21372032be2bdc529e48ff9abdd5c6
Heeyeun Ko <koheeyeun96@gmail.com>
2024-10-09 23:22:04 +0900

    style: ChatInputBox

--- src/chat/components/ChatInputBox.jsx
+++ src/chat/components/ChatInputBox.jsx
@@ -4,7 +4,7 @@ import {Send} from "lucide-react";
 export default function ChatInputBox(message, setMessage, onSendClicked) {
     return (
         <Wrapper>
-        <input 
+        <SendInput 
         placeholder="답장을 입력해주세요"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
@@ -14,16 +14,40 @@ export default function ChatInputBox(message, setMessage, onSendClicked) {
             }
         }}
         />
-        <Send/>
+        <CircleSend>
+        <Send color="white"/>
+        </CircleSend>
         </Wrapper>
     );
 }
+
+const SendInput = styled.input`
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    font-size: 18px;
+    background-color: transparent;
+`;
 
 const Wrapper = styled.div`
     height: 60px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin: 0 15px 0 15px;
     padding: 0 20px 0 20px;
+    border-radius: 30px;
     background-color: #ECECEC;
+`;
+
+const CircleSend = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #F08A5D;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
 `;
--- src/chat/pages/ChatPage.jsx
+++ src/chat/pages/ChatPage.jsx
@@ -1,12 +1,23 @@
 import styled from "styled-components";
 import ChatbotIcon from "@/assets/ChatbotIcon.svg";
 import UserIcon from "@/assets/UserIcon.svg";
+import { useState } from "react";
+import ChatInputBox from "@/chat/components/ChatInputBox.jsx";
 
 export default function ChatPage() {
+  const [toSendMessage, setToSendMessage] = useState("");
+  const onSendClicked = () => {
+    console.log(toSendMessage);
+    setToSendMessage("");
+  }
+
+
   return (
    <Wrapper>
       <h1>Chat Page</h1>
+      <ChatInputBox message={toSendMessage} setMessage={setToSendMessage} onSendClicked={onSendClicked} />
     </Wrapper>
+    
   );
 }
 
