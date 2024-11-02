5487144f744b98d466a2b97cb9e6c11d683efd58
차예원 <chayewon@chayewon-ui-noteubug.local>
2024-11-02 19:42:38 +0900

    feat: modify personal alarm and chart

--- src/supervisor/chart/components/PersonalChart.jsx
+++ src/supervisor/chart/components/PersonalChart.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import { ReponsiveBar, ResponsiveBar } from "@nivo/bar";
+import { ResponsiveBar } from "@nivo/bar";
 import styled from "styled-components";
 
-export default function PersonalChart({ data }) {
-  return (
-    <ChartWrapper>
-      <ResponsiveBar
-        data={sampleData}
-        keys={["responseRate"]}
-        indexBy="date"
-        colors={({ data }) =>
-          data.responseRate === 0
-            ? "#FF6347"
-            : data.responseRate <= 50
-            ? "#FFA500"
-            : "#32CD32"
-        }
-        {...chartSettings}
-      ></ResponsiveBar>
-    </ChartWrapper>
-  );
-}
+// 이름은 순서 따로 없이 리스트로, 응답률은 이름 순서에 맞춰서 리스트로
 
 const getDayOfWeek = (dateString) => {
   const date = new Date(dateString);
-  return date.toLocalDateString("ko-KR", { weekday: "short" });
+  return date.toLocaleDateString("ko-KR", { weekday: "short" });
 };
+
+const sampleUserName = "차예원";
 
 const sampleData = [
   { date: "2024-10-26", responseRate: 30 },
@@ -35,37 +19,69 @@ const sampleData = [
   { date: "2024-10-30", responseRate: 0 },
   { date: "2024-10-31", responseRate: 100 },
   { date: "2024-11-01", responseRate: 40 },
-];
+].map((item) => ({
+  ...item,
+  day: getDayOfWeek(item.date),
+}));
+
+export default function PersonalChart({ data, userName = sampleUserName }) {
+  return (
+    <ChartWrapper>
+      <Title>{userName}님의 응답률</Title>
+      <ResponsiveBar
+        data={sampleData}
+        keys={["responseRate"]}
+        indexBy="day"
+        colors={({ data }) =>
+          data.responseRate === 0
+            ? "#FF6347"
+            : data.responseRate <= 50
+            ? "#FFA500"
+            : "#32CD32"
+        }
+        {...chartSettings}
+        width={250}
+        height={205}
+      />
+    </ChartWrapper>
+  );
+}
 
 const ChartWrapper = styled.div`
   width: 250px;
   height: 250px;
-  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  justify-contents: center;
+  align-items: center;
   background-color: #f5f5f5;
+  border-radius: 20px;
+`;
+
+const Title = styled.h3`
+  height: 40px;
+  text-align: center;
+  margin: 10px 0 0 0;
+  font-size: 1.2rem;
 `;
 
 const chartSettings = {
-  margin: { top: 20, right: 20, bottom: 20, left: 30 },
+  margin: { top: 10, right: 20, bottom: 20, left: 30 },
   padding: 0.2,
   axisBottom: {
     tickSize: 3,
     tickPadding: 3,
     tickRotation: 0,
-    legend: "Date",
-    legendPosition: "middle",
-    legendOffset: 20,
   },
   axisLeft: {
-    tickSize: 5,
-    tickPadding: 5,
+    tickSize: 3,
+    tickPadding: 3,
     tickRotation: 0,
-    legend: "Response Rate (%)",
-    legendPosition: "middle",
-    legendOffset: -40,
+    tickValues: [0, 25, 50, 75, 100],
   },
-  labelSkipWidth: 12,
-  labelSkipHeight: 12,
-  labelTextColor: { from: "color", modifiers: [["darker", 1.6]] },
+  labelSkipWidth: 16,
+  labelSkipHeight: 16,
+  labelTextColor: { from: "color", modifiers: [["darker", 1.5]] },
   animate: true,
   motionStiffness: 90,
   motionDamping: 15,
