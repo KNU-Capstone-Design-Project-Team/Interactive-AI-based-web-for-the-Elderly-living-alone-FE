a7d32cab5be301b48148ca51f0999b72c26c99cb
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-03 14:23:48 +0900

    feat: router

--- src/global/const/const.js
+++ src/global/const/const.js
@@ -1,7 +1,12 @@
 export const ROUTER_PATH = {
     HOME: "/",
+    SENIORHOME: "/senior",
+    SUPERHOME: "/supervisor",
     RECOMMEND: "/recommend",
     CHAT: "/chat",
+    ALARM: "/alarm",
+    CHART: "/chart",
+
 };
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
