fabfaed4f6a88db9af633b102b19d6ba1656ee62
차예원 <chayewon@chayewon-ui-noteubug.local>
2024-11-07 19:58:58 +0900

    feat: hook in chart and alarm page

--- src/supervisor/alarm/pages/AlarmPage.jsx
+++ src/supervisor/alarm/pages/AlarmPage.jsx
@@ -1,17 +1,65 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 import PersonalAlarm from "@/supervisor/alarm/components/PersonalAlarm";
+import axios from "axios";
+import { API_BASE_URL } from "@/global/const/const";
 
 export default function AlarmPage() {
+  // const [todayDate, setTodayDate] = useState([]);
+  // const [todayAlarmList, setTodayAlarmList] = useState([]);
+  // const [loginId, setLoginId] = useState("sampleLoginId");
+
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `${API_BASE_URL}/supervisor/${loginId}/notice`
+  //     );
+  //     setTodayDate(response.data.todayDate);
+  //     setTodayAlarmList(response.data.todayAlarmList);
+  //   } catch (error) {}
+  // };
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch("/notice");
+  //     const data = await response.json();
+  //     setTodayAlarmList(data);
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  const todayDate = "2024.11.04";
   const todayAlarmList = [
-    ("고희연", 75),
-    ("김채은", 0),
-    ("이혜빈", 32.5),
-    ("차예원", 50),
+    ["고희연", 75],
+    ["김채은", 0],
+    ["이혜빈", 32.5],
+    ["차예원", 50],
+    ["고희연", 75],
+    ["김채은", 0],
+    ["이혜빈", 32.5],
+    ["차예원", 50],
+    ["고희연", 75],
+    ["김채은", 0],
+    ["이혜빈", 32.5],
+    ["차예원", 50],
+    ["고희연", 75],
+    ["김채은", 0],
+    ["이혜빈", 32.5],
+    ["차예원", 50],
   ];
+
   return (
     <Wrapper>
+      <Date>{todayDate}</Date>
       <AlarmList>
-        <PersonalAlarm></PersonalAlarm>
+        {todayAlarmList.map(([userName, responseRate]) => (
+          <PersonalAlarm
+            key={userName}
+            userName={userName}
+            responseRate={responseRate}
+          ></PersonalAlarm>
+        ))}
       </AlarmList>
     </Wrapper>
   );
@@ -21,6 +69,7 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 20px;
@@ -28,13 +77,17 @@ const Wrapper = styled.div`
 `;
 
 const AlarmList = styled.div`
+  width: 90%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
   overflow-y: auto;
-  max-height: 80vh;
-  padding-right: 15px;
+  max-height: 70vh;
+  padding-right: 10px;
   box-sizing: border-box;
-  width: 280px;
+`;
+
+const Date = styled.h3`
+  text-align: center;
+  font-size: 1.2rem;
 `;
