71b63aa5eb9ec779caa0c06e7914714469d38638
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-07 22:33:53 +0900

    feat: 필요한 Context Provider 다 구현

--- src/start/context/SeniorContext.jsx
+++ src/start/context/SeniorContext.jsx
@@ -3,12 +3,31 @@ import React, { createContext, useState } from "react";
 export const SeniorContext = createContext();
 
 export const SeniorContextProvider = ({ children }) => {
-
+    const [birthYear, setBirthYear] = useState(null);
+    const [birthMonth, setBirthMonth] = useState(null);
+    const [birthDay, setBirthDay] = useState(null);
+    const [city, setCity] = useState("");
+    const [gu, setGu] = useState("");
+    const [dong, setDong] = useState("");
+    const [categories, setCategories] = useState([]);
 
   return (
     <SeniorContext.Provider 
     value={{ 
-
+        birthYear,
+        setBirthYear,
+        birthMonth,
+        setBirthMonth,
+        birthDay,
+        setBirthDay,
+        city,
+        setCity,
+        gu,
+        setGu,
+        dong,
+        setDong,
+        categories,
+        setCategories
        }}>
       {children}
     </SeniorContext.Provider>
